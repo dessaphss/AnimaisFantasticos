be4d4a1e3b2c03ffab80f0dd7a9cb5905149aff1
Andressa Paranhos <andressaparanhos1@hotmail.com>
2022-12-27 16:48:16 -0300

    add: função scroll suave

--- script.js
+++ script.js
@@ -50,3 +50,23 @@ function initAccordion(){
 }
 initAccordion();
 
+//Função Scroll suave
+function initScrollSuave(){
+  const linksInterno = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
+
+  linksInterno.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
